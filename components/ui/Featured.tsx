add834ea7ecc9859d91c65237d175bda94bd6f78
Taylor Winowiecki <trw0511@yahoo.com>
2023-03-19 22:34:17 -0400

    stuff

--- components/ui/Featured.tsx
+++ components/ui/Featured.tsx
@@ -12,7 +12,11 @@ const Featured = ({ name, products, relatedObjs }: FeaturedProps) => {
   return (
     <>
       <h2 className="tracking-widest">{name.toUpperCase()}</h2>
-      <div className="flex justify-center w-full max-w-full gap-4 pt-4 overflow-y-auto">
+      <div
+        className={`${
+          products.length === 1 ? 'justify-center' : ''
+        } flex w-full max-w-full gap-4 pt-4 overflow-y-auto`}
+      >
         {products.map(product => (
           <div key={product.id}>
             <ProductCard item={product} relatedObj={relatedObjs} />
